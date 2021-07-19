9107feb05812d9130f03802113c889b899d788bc
AleksPet <p.aleksandra83@gmail.com>
2021-07-19 16:42:59 -0400

    connecting git hub and ij

--- src/gitCool.java
+++ src/gitCool.java
@@ -1,5 +1,10 @@
 public class gitCool {
+    public static void main(String[] args) {
+
+    }
     // b23 is learning git
     //b23 is making some sense now
     //b 23 is special number
+ // just commect git and iJ
 }
+
